6d3edaef394b52c05ef9192256cf2b8419e0a53e
Roselyn Guzman <roselyn.1768260@nv.ccsd.net>
2020-09-27 20:05:43 +0000

    Updated the code for HowFarAway

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -1,9 +1,35 @@
+//Imports the Scanner
 import java.util.Scanner;
 
 public class HowFarAway 
 {
     public static void main(String[] args)
     {
-        // your code here.
-    }
-}
+      //Adds a new Scanner
+      Scanner in = new Scanner(System.in);
+
+      //Provides a variable of latitude and longitude
+      double theLatitude, theLongitude;
+
+      /*Asks the user to input the latitude and longitude
+      for both the starting location and the ending location*/
+      System.out.println("Enter the latitude of the starting location: ");
+      theLatitude = in.nextDouble();
+      System.out.println("Enter the longitude of the starting location: ");
+      theLongitude = in.nextDouble();
+      System.out.println("Enter the latitude of the ending location: ");
+      theLatitude = in.nextDouble();
+      System.out.println("Enter the longitude of the ending location: ");
+      theLongitude = in.nextDouble();
+
+      //Adds a new construction
+      GeoLocation philip = new GeoLocation(theLatitude, theLongitude);
+
+      //Computes the distance in miles between the two locations
+      
+      
+      //Prints out the distance
+
+
+    }//Ends the main constructor
+}//Ends the HowFarAway class
